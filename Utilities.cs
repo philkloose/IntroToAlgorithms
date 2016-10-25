9304d267c8c5e52321eafdc3de3f144052780d38
philkloose <philip.kluss@gmail.com>
2016-10-25 13:34:09 -0500

    overloaded CreateUnsortedArray

--- Utilities.cs
+++ Utilities.cs
@@ -1,17 +1,28 @@
 using System;
 public class Utilities {
 
-    public static int[] CreateUnsortedArray(int length) {
+    public static int[] CreateUnsortedArray (int length, int min, int max) {
 
         int[] array = new int[length];
         Random r = new Random();
         for (int i = 0; i < array.Length; i++) {
-            //array[i] = r.Next(Int32.MinValue, Int32.MaxValue);
-            array[i] = r.Next(0, 100);
+            array[i] = r.Next(min, max);
         }
 
         return array;
     }
+
+    public static int[] CreateUnsortedArray (int length) {
+
+        int[] array = new int[length];
+        Random r = new Random();
+        for (int i = 0; i < array.Length; i++) {
+            array[i] = r.Next(Int32.MinValue, Int32.MaxValue);
+        }
+
+        return array;
+    }
+
     public static void PrintArray (int[] a) {
         for (int i = 0; i < a.Length; i++) {
             if (i != a.Length - 1)
@@ -23,6 +34,7 @@ public class Utilities {
         }
         Console.WriteLine();
     }
+    
     public static string ReturnArrayAsString (int[] a) {
         string s = "[";
 
